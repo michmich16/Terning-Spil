77c329567a5f8ee88d3f22205fadfa3e8c2eceaf
Michael P <102651473+michmich16@users.noreply.github.com>
2024-08-28 09:52:48 +0200

    dices added

--- terning-react/src/components/Dice/Dice.jsx
+++ terning-react/src/components/Dice/Dice.jsx
@@ -1,1 +1,17 @@
 import s from './Dice.module.scss';
+import dice1 from '../../assets/images/dice1.svg'
+import dice2 from '../../assets/images/dice2.svg'
+import dice3 from '../../assets/images/dice3.svg'
+import dice4 from '../../assets/images/dice4.svg'
+import dice5 from '../../assets/images/dice5.svg'
+import dice6 from '../../assets/images/dice6.svg'
+
+const diceImages = [dice1, dice2, dice3, dice4, dice5, dice6];
+
+const Dice = ({number}) =>{
+    return (
+        <img src={diceImages[number -1]} alt={`Dice${number}`} />
+    );
+};
+
+export default Dice;
